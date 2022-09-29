06e0edd9ae154cdbce4d6f53fa93aaed2c743d92
liqianbmw <liqianbmw@163.com>
2022-09-29 11:18:59 +0800

    commit version

--- website/settings/dev.py
+++ website/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 # DEBUG=False
-INTERNAL_IPS = ['127.0.0.1', ]#debug_toolbar
 DEBUG=True
+INTERNAL_IPS = ['127.0.0.1', ]#debug_toolbar
 ALLOWED_HOSTS = ['*']
 JADX_HOME = 'jadx -d '
 APKTOOL_HOME="/Users/qianli/workspace/python/www/tools/apktool_2.6.1.jar";
@@ -10,7 +10,7 @@ DOWN_FILE ="/Users/qianli/workspace/python/www/tools/down"
 
 # STATICFILES_DIRS = [ os.path.join(CORE_DIR, "../static/"),]
 STATICFILES_DIRS = [ "/Users/qianli/workspace/python/www/decompiler.anytools.online/website/static"]
-# STATIC_URL = '/static/'
-STATIC_URL = '/Users/qianli/workspace/python/www/decompiler.anytools.online/website/static/'
+STATIC_URL = '/static/'
+# STATIC_URL = '/Users/qianli/workspace/python/www/decompiler.anytools.online/website/static/'
 # STATIC_ROOT = os.path.join(CORE_DIR, "static")
 STATIC_ROOT = "/Users/qianli/workspace/python/www/decompiler.anytools.online/static";
